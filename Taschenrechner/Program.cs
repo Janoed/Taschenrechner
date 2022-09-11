a16c7548d2dcf7fc942d24821b9a4f40f8731250
Janoed <100380898+Janoed@users.noreply.github.com>
2022-09-11 19:52:46 +0200

    Projektdateien hinzufügen.

---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TaschenRechner1
+{
+    class Rechner
+    {
+        static void Main(string[] args)
+        {
+
+            double num1 = 0;
+            double num2 = 0;
+            double result = 0;
+            
+            Console.WriteLine("Erster Taschenrechner auf C#");
+
+            Console.WriteLine("Enter number 1");
+            num1= Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Enter number 2");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
