729fe232eab10d6846d5427d7f7c339385a9d30b
arber.misini <arber.misini@gmx.ch>
2022-09-12 22:47:00 +0200

    Switch and case result

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -12,12 +12,42 @@ namespace TaschenRechner1
             double result = 0;
             
             Console.WriteLine("Erster Taschenrechner auf C#");
+            Console.WriteLine("---------------------------------------");
 
+            Console.WriteLine("Option");
+            Console.WriteLine("---------------------------------------");
+
+           
             Console.WriteLine("Enter number 1");
             num1= Convert.ToDouble(Console.ReadLine());
             
             Console.WriteLine("Enter number 2");
             num2 = Convert.ToDouble(Console.ReadLine());
+
+
+           
+            Console.WriteLine("Weli recheoperation wetsch");
+            Console.WriteLine("+ : Add");
+            Console.WriteLine("- : subtract");
+
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+                    break;
+
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine($"du Kalb: {num1} - {num2} = " + result);
+                    break;
+
+                default:
+                    Console.WriteLine("That was not a valid option");
+                    break;
+            }
+
+            
 
             Console.ReadKey();
 
